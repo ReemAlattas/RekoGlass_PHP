3e9b915c128c76bda6ccf583833ac93831aa6b59
Tianqiang Liu <tqliu@orbe.us>
2014-05-09 20:49:08 -0700

    v1.11
    Fix the bug of adapting new Mirror sdk

--- rekognition/config.php
+++ rekognition/config.php
@@ -2,9 +2,9 @@
               
 //You need to register an api key at https://rekognition.com
 
-$rekognition_api_key = 'aqMzHHugfu1eQvW2';
-$rekognition_api_secret = '3AQ2Mb0RLquuQ7eR';
+$rekognition_api_key = 'YOUR_REKOGNITION_API_KEY'
+$rekognition_api_secret = 'YOUR_REKOGNITION_API_SECRET';
 $rekognition_jobs = 'face_part_gender_emotion_age_glass_mouth,scene'; //Your jobs
 $rekognition_name_space = '';
 $rekognition_user_id = ''; 
-$GLOBALS['REKOGNITION_ROOT'] = "/data/www/ggtest/rekognition/";
+$GLOBALS['REKOGNITION_ROOT'] = "YOUR/PATH/TO/REKOGNITION/FOLDER";
